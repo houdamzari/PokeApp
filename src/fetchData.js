995f20646a10360e59a38860fcd66fd3ef0452a6
houdamzari <houdamzari1999@gmail.com>
2023-01-03 14:41:28 +0100

    displaying grid data

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
-
+let element = '';
+const grid = document.querySelector('.grid');
 const gridLogic = (data) => {
-  let element = '';
   element += `<div>
   <div class="imageContainer"><img src=${data.sprites.front_default} /></div>
   <h3>${data.species.name}</h3>
   <div class="buttonContainer"><button>Comments</button><button>Reservation</button></div>
   </div>`;
-  grid.innerHtml = element;
+  grid.innerHTML = element;
 };
 
 export const fetchData = () => {
-  const grid = document.querySelector('.grid');
   axios
     .get(' https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
     .then((res) => {
       const data = res.data.results;
 
       data.forEach((el) => {
-        axios.get(el.url).then((res) => console.log(res.data));
+        axios.get(el.url).then((res) => gridLogic(res.data));
       });
     });
 };
