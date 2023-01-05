4fca8bafd846d24dec0412e26b8659756ad3aed3
PrantoshB <Pbiswas1007@gmail.com>
2023-01-05 20:05:28 +0530

    Style the Pop up and add colors to the elements

--- src/modules/fetchData.js
+++ src/modules/fetchData.js
@@ -2,18 +2,88 @@ import axios from 'axios';
 
 let element = '';
 const grid = document.querySelector('.grid');
+const appId = '91WxoTjxGWkdx6jiMCZQ';
+const baseURL = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appId}`;
+const SubmitBtn = document.querySelector('.submit-comment');
+const Name = document.querySelector('#name');
+const Comment = document.querySelector('#comment');
+const PopUp = document.querySelector('.popup');
+const CloseBtn = document.querySelector('.closebtn');
+const MainBody = document.querySelector('.main');
+const NewComments = document.querySelector('.newComment');
+
+//* ***fetch comment and show to the popup */
+const fetchComment = async (itemId) => {
+  const response = await fetch(`${baseURL}/comments?item_id=${itemId}`);
+  const data = await response.json();
+  const Comments = await data;
+  NewComments.innerHTML = '';
+  if (response.status === 200) {
+    Comments.forEach((element) => {
+      NewComments.innerHTML += `
+            <li>${element.creation_date} ${element.username} : ${element.comment}</li>
+            `;
+    });
+  } else {
+    NewComments.innerHTML = '';
+  }
+};
+
+//* **Add comment function*/
+const addComment = async (itemId, username, comment) => {
+  await fetch(`${baseURL}/comments?item_id=${itemId}`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      comment,
+    }),
+  });
+  fetchComment(itemId);
+};
+
+//* **Send comments to the API : depends on addComment function*/
+SubmitBtn.addEventListener('click', () => {
+  const reqId = SubmitBtn.getAttribute('id');
+  addComment(reqId, Name.value, Comment.value);
+  Name.value = '';
+  Comment.value = '';
+});
+
+//* **Close button behaviour */
+CloseBtn.addEventListener('click', () => {
+  PopUp.classList.remove('showpop');
+  NewComments.innerHTML = '';
+  MainBody.style.filter = 'blur(0)';
+});
+
+//* **Load data after fetching data from Poke API */
 const gridLogic = (data) => {
   element += `<div class="grid-item">
  <img class="pokeimg" src=${data.sprites.other['official-artwork'].front_default} />
   <h3 class="pokename">${data.species.name}</h3>
-  <div class="buttonContainer"><button type="button" class="commentbtn">Comments</button></div>
+  <div class="buttonContainer"><button class="commentbtn" id="${data.species.name}">Comment</button></div>
   </div>`;
   grid.innerHTML = element;
+  const commentButton = document.querySelectorAll('.commentbtn');
+
+  //* **Comment button behaviour inside the Pokemon Cards */
+  commentButton.forEach((element) => element.addEventListener('click', (e) => {
+    const commentId = e.target.id;
+    PopUp.classList.add('showpop');
+    MainBody.style.filter = 'blur(10px)';
+    fetchComment(commentId);
+    SubmitBtn.setAttribute('id', commentId);
+  }));
 };
 
-export const fetchData = async () => {
-  await axios
-    .get(' https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+//* **Fetch Pokemons from Poke API */
+export const fetchData = () => {
+  axios
+    .get(' https://pokeapi.co/api/v2/pokemon?limit=40&offset=0')
     .then((res) => {
       const data = res.data.results;
       data.forEach((el) => {
